ce1b2772d59fcf9d13c3f122058915533f9a95b7
ajayleodas <ns.ajaysahadevan@gmail.com>
2025-10-31 11:17:48 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,12 @@
+class product{
+    int product_id;
+    String product_name;
+    int product_price;
+}
+void product(int id,String name,int price )
+{
+    product_id=id;
+    product_name=name;
+    product_price=price;
+
+}
